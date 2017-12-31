ccd8d1a680cad232ca2a50a4d7f9800551776394
MrCmonster <DavidOreol@gmail.com>
2017-12-31 15:31:09 -0700

    Initial Upload

---
+++ David_Weather_OLED/David_Weather_OLED.ino
@@ -0,0 +1,242 @@
+/***************************************************************************
+  This is a library for the BME280 humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BME280 Breakout
+  ----> http://www.adafruit.com/products/2650
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface. The device's I2C address is either 0x76 or 0x77.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+/*
+
+  PrintTest.pde
+
+  How to use the base class "Print"
+
+  >>> Before compiling: Please remove comment from the constructor of the
+  >>> connected graphics display (see below).
+
+  Universal 8bit Graphics Library, https://github.com/olikraus/u8glib/
+
+  Copyright (c) 2012, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list
+    of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+#include "U8glib.h"
+
+// setup u8g object, please remove comment from one of the following constructor calls
+// IMPORTANT NOTE: The following list is incomplete. The complete list of supported
+// devices with all constructor calls is here: https://github.com/olikraus/u8glib/wiki/device
+//U8GLIB_SH1106_128X64 u8g(13, 11, 10, 9);  // SW SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9
+//U8GLIB_SH1106_128X64 u8g(4, 5, 6, 7); // SW SPI Com: SCK = 4, MOSI = 5, CS = 6, A0 = 7 (new blue HalTec OLED)
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE); // I2C / TWI
+//U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_FAST); // Dev 0, Fast I2C / TWI
+//U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NO_ACK); // Display which does not send ACK
+
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+unsigned long delayTime;
+int fontLineSpacing = 0; //default to 0, read later
+float currentTemp = 0;
+float currentHumidity = 0;
+float currentPressure = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  if (! bme.begin()) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+/*/
+  Serial.println("-- Default Test --");
+  Serial.println("normal mode, 16x oversampling for all, filter off,");
+  Serial.println("0.5ms standby period");
+  delayTime = 10;
+//*/
+
+  // For more details on the following scenarious, see chapter
+  // 3.5 "Recommended modes of operation" in the datasheet
+
+  //*
+      // weather monitoring
+      Serial.println("-- Weather Station Scenario --");
+      Serial.println("forced mode, 1x temperature / 1x humidity / 1x pressure oversampling,");
+      Serial.println("filter off");
+      bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                      Adafruit_BME280::SAMPLING_X1, // temperature
+                      Adafruit_BME280::SAMPLING_X1, // pressure
+                      Adafruit_BME280::SAMPLING_X1, // humidity
+                      Adafruit_BME280::FILTER_OFF,
+                      Adafruit_BME280::STANDBY_MS_10);
+
+      // suggested rate is 1/60Hz (1m)
+      delayTime = 10; // in milliseconds
+  //*/
+
+  /*
+      // humidity sensing
+      Serial.println("-- Humidity Sensing Scenario --");
+      Serial.println("forced mode, 1x temperature / 1x humidity / 0x pressure oversampling");
+      Serial.println("= pressure off, filter off");
+      bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                      Adafruit_BME280::SAMPLING_X1,   // temperature
+                      Adafruit_BME280::SAMPLING_NONE, // pressure
+                      Adafruit_BME280::SAMPLING_X1,   // humidity
+                      Adafruit_BME280::FILTER_OFF );
+
+      // suggested rate is 1Hz (1s)
+      delayTime = 1000;  // in milliseconds
+  */
+
+  /*
+      // indoor navigation
+      Serial.println("-- Indoor Navigation Scenario --");
+      Serial.println("normal mode, 16x pressure / 2x temperature / 1x humidity oversampling,");
+      Serial.println("0.5ms standby period, filter 16x");
+      bme.setSampling(Adafruit_BME280::MODE_NORMAL,
+                      Adafruit_BME280::SAMPLING_X2,  // temperature
+                      Adafruit_BME280::SAMPLING_X16, // pressure
+                      Adafruit_BME280::SAMPLING_X1,  // humidity
+                      Adafruit_BME280::FILTER_X16,
+                      Adafruit_BME280::STANDBY_MS_0_5 );
+
+      // suggested rate is 25Hz
+      // 1 + (2 * T_ovs) + (2 * P_ovs + 0.5) + (2 * H_ovs + 0.5)
+      // T_ovs = 2
+      // P_ovs = 16
+      // H_ovs = 1
+      // = 40ms (25Hz)
+      // with standby time that should really be 24.16913... Hz
+      delayTime = 41;
+
+      /*
+      // gaming
+      Serial.println("-- Gaming Scenario --");
+      Serial.println("normal mode, 4x pressure / 1x temperature / 0x humidity oversampling,");
+      Serial.println("= humidity off, 0.5ms standby period, filter 16x");
+      bme.setSampling(Adafruit_BME280::MODE_NORMAL,
+                      Adafruit_BME280::SAMPLING_X1,   // temperature
+                      Adafruit_BME280::SAMPLING_X4,   // pressure
+                      Adafruit_BME280::SAMPLING_NONE, // humidity
+                      Adafruit_BME280::FILTER_X16,
+                      Adafruit_BME280::STANDBY_MS_0_5 );
+
+      // Suggested rate is 83Hz
+      // 1 + (2 * T_ovs) + (2 * P_ovs + 0.5)
+      // T_ovs = 1
+      // P_ovs = 4
+      // = 11.5ms + 0.5ms standby
+      delayTime = 12;
+  */
+
+
+  u8g.setFont(u8g_font_unifont);
+  fontLineSpacing = u8g.getFontLineSpacing() + 1;
+
+  Serial.print("fontLineSpacing = ");
+  Serial.println(fontLineSpacing, DEC);
+  Serial.println("Finished setup()");
+}
+
+int counter = 0;
+
+void loop() {
+  // Only needed in forced mode! In normal mode, you can remove the next line.
+  bme.takeForcedMeasurement(); // has no effect in normal mode
+
+  // picture loop for OLED
+  u8g.firstPage();
+  do {
+    if (counter == 10)
+    {
+      getValues();
+      counter = 0;
+    }
+    printValues();
+    delay(delayTime);
+  } while ( u8g.nextPage() );
+
+counter++;
+}
+
+
+void printValues() {
+  // graphic commands to redraw the complete screen should be placed here
+
+//  u8g.setFont(u8g_font_unifont);
+  u8g.setPrintPos(0, fontLineSpacing);
+  // call procedure from base class, http://arduino.cc/en/Serial/Print
+  u8g.print("Temp:");
+  u8g.print(currentTemp, 1);
+  u8g.print("\260F"); // '\260' is the ASCII degree symbol
+
+  u8g.setPrintPos(0, fontLineSpacing*2);
+  u8g.print("Press:");
+  u8g.print(currentPressure / 100.0F, 1);
+  u8g.print(" hPa");
+
+  u8g.setPrintPos(0, fontLineSpacing*3);
+  u8g.print("Hum:");
+  u8g.print(currentHumidity, 1);
+  u8g.print("%");
+
+}
+
+void getValues() {
+  currentTemp = bme.readTemperature() * 9/5 + 32;
+  currentPressure = bme.readPressure();
+  currentHumidity = bme.readHumidity();
+}
+
+
